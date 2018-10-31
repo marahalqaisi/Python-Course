8c8fa32c82560c834cdadf9305705ab12935ef40
marahalqaisi <44285832+marahalqaisi@users.noreply.github.com>
2018-10-31 18:43:07 +0200

    Add files via upload

---
+++ Week 2/Practice 2-C.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+#Practice 2-C
+
+import numpy
+
+numbers = [23,76,97,28,10,8,2,4,6]
+print (numbers)
+
+mean = numpy.mean (numbers)
+print ("Mean= ",mean)
+
+median = numpy.median (numbers)
+print ("Median= ",median)
+
+standard_dev = numpy.std (numbers)
+print ("Standard deviation= ",standard_dev)
+
+
